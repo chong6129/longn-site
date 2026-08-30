593ea343a1fad14985490420943e0fa47d113db6
Long Nguyen <chong6129@hotmail.com>
2026-08-30 08:43:51 -0400

    Add site routes and shared base template
    Add site routes and shared base template

--- app.py
+++ app.py
@@ -8,5 +8,15 @@ def home():
     return render_template("index.html")
 
 
+@app.route("/blog")
+def blog():
+    return render_template("blog.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
 if __name__ == "__main__":
     app.run(debug=True)
